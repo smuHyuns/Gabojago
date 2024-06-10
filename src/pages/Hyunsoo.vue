1b75a84ff4ee5fd3212a87a1fd438a69b3ea0c64
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-06-10 17:48:14 +0900

    root color 업데이트

--- src/pages/Hyunsoo.vue
+++ src/pages/Hyunsoo.vue
@@ -6,4 +6,25 @@
 
 <script setup></script>
 
-<style lang="scss" scoped></style>
+<style scoped>
+:root {
+  --grey-0: #ffffff;
+  --grey-100: #fbfbfc;
+  --grey-200: #f5f6f7;
+  --grey-300: #eaecef;
+  --grey-400: #caced4;
+  --grey-600: #8892a0;
+  --grey-700: #616b79;
+  --grey-800: #3e444e;
+  --grey-900: #353b43;
+  --grey-950: #16181c;
+  --blue-0: #eff4ff;
+  --blue-100: #b6caff;
+  --blue-200: #84a7ff;
+  --blue-300: #5d8bff;
+}
+
+h1 {
+  color: var(--blue-300);
+}
+</style>
