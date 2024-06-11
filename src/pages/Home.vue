65251925fcfe76f9562545724c4651694f46829a
NaGU <kwna0729@naver.com>
2024-06-11 16:04:53 +0900

    Committing local changes before pulling

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,18 +1,40 @@
 <template>
   <div>
     <Topbar titleText="여행지 제목" />
-    <VDatePicker v-model="date" class="calendar" />
+  </div>
+  <div class="calendar-container">
+    <!-- VCalendar 컴포넌트로 변경 -->
+    <VCalendar v-model="date" class="calendar" />
   </div>
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import Topbar from '@/components/Topbar.vue';
-import 'v-calendar/style.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+// VCalendar 컴포넌트를 임포트
+import VCalendar from 'v-calendar';
+
+const date = ref(null);
 </script>
 
 <style scoped>
+.calendar-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: calc(100vh - 60px); /* 화면 높이의 전체에서 Topbar의 높이를 뺀 값 */
+}
+
 .calendar {
-  width: 1000px;
-  height: 1000px;
+  width: 100%;
+  max-width: none; /* 최대 너비 제한을 없애서 화면 크기에 맞게 조정될 수 있도록 함 */
+  height: auto; /* 높이를 자동으로 설정하여 내용에 맞게 조정될 수 있도록 함 */
+}
+
+@media (max-width: 1080px) {
+  .calendar {
+    width: 100%; /* 모바일 화면에서도 달력의 너비를 100%로 설정 */
+  }
 }
 </style>
--- src/pages/Hyunsoo.vue
+++ src/pages/Hyunsoo.vue
@@ -4,7 +4,12 @@
     <div class="imgContainer">
       <img src="../assets/비행기토끼.png" class="rabbitImg" />
     </div>
-    <InfoBox class="infoBox" firstInput="돈 관리는 저희가 도와드릴게요" secondInput="어디로 떠나시나요?" thirdInput="여행 등록하기 >" />
+    <InfoBox
+      class="infoBox"
+      firstInput="돈 관리는 저희가 도와드릴게요"
+      secondInput="어디로 떠나시나요?"
+      thirdInput="여행 등록하기 >"
+    />
   </div>
   <div class="whiteBox">
     <div class="kindTripBox">
@@ -27,7 +32,9 @@
       </ul>
     </div>
     <div class="planListBox">
-      <div class="noneBox">여행 중인 곳이 없습니다<br />여행을 등록해주세요</div>
+      <div class="noneBox">
+        여행 중인 곳이 없습니다<br />여행을 등록해주세요
+      </div>
     </div>
     <img src="../assets/addPlanBtn.png" class="addPlanBtn" />
   </div>
@@ -81,8 +88,14 @@ import InfoBox from '@/components/InfoBox.vue';
   height: 430px;
   bottom: 0; /* imgContainer의 밑부분을 완전히 덮도록 설정 */
   width: 100%; /* infoBox가 BlueBox의 전체 너비를 차지하도록 설정 */
+
   margin: 0;
-  background: linear-gradient(180deg, rgba(123, 160, 255, 0) 0%, #749bff 36%, #5d8bff 100%);
+  background: linear-gradient(
+    180deg,
+    rgba(123, 160, 255, 0) 0%,
+    #749bff 36%,
+    #5d8bff 100%
+  );
 }
 
 .whiteBox {
