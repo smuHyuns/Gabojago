e481b209f375d5bf59a34d5c0ae94ef2e275d4c5
NaGU <kwna0729@naver.com>
2024-06-11 12:27:09 +0900

    2024.06.11 영상 제작 완료(coin_spin, slide)

--- src/pages/Gun.vue
+++ src/pages/Gun.vue
@@ -1,6 +1,10 @@
 <template>
   <div>
     <h1>건우페이지</h1>
+    <!-- <video autoplay muted loop class="background-video">
+        <source src="./assets/slide.webm" type="video/webm" />
+      </video> -->
+    <!-- webm 동영상 넣는 방법입니다! -->
   </div>
 </template>
 
