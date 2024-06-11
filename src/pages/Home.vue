8009873c50c1a0c31ada6f816f05ec6946496391
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-06-11 14:16:28 +0900

    20240611 말투수정 없어요

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,22 @@
 <template>
   <div>
     <Topbar titleText="여행지 제목" />
-    <VDatePicker v-model="date" class="calendar" />
+    <div class="calendar-container">
+      <VDatePicker v-model="date" expanded />
+    </div>
   </div>
 </template>
 
 <script setup>
 import Topbar from '@/components/Topbar.vue';
+import { ref } from 'vue';
 import 'v-calendar/style.css';
+
+const date = ref(null);
 </script>
 
 <style scoped>
-.calendar {
+.calendar-container {
   width: 1000px;
   height: 1000px;
 }
