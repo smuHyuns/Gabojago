02b350a909bf1ef737a62aaff544a053a2438db8
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-06-11 11:54:10 +0900

    Revert "20240611 CalendarTopbar.vue 추가"
    This reverts commit a6f715b74fc6e251310dda2240316c49c3820da1.

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,17 +1,11 @@
 <template>
   <div>
-    <Topbar titleText="여행지 제목" class="topbar" />
-    <CalendarTopbar titleText="2024년 5월" />
+    <Topbar titleText="여행지 제목" />
   </div>
 </template>
 
 <script setup>
 import Topbar from '@/components/Topbar.vue';
-import CalendarTopbar from '@/components/CalendarTopbar.vue';
 </script>
 
-<style scoped>
-.topbar {
-  margin-bottom: 50px;
-}
-</style>
+<style scoped></style>
