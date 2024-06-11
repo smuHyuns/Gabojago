68ceebc3314ff0c3faaa0ca42cb17e465013b3f9
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-06-11 15:10:26 +0900

    20240611

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,22 +1,42 @@
 <template>
   <div>
-    <Topbar titleText="여행지 제목" />
+    <Topbar titleText="여행지 제목" class="topbar" />
+    <CalendarTopbar titleText="2024년 5월" class="calendarTopbar" />
     <div class="calendar-container">
-      <VDatePicker v-model="date" expanded />
+      <h3><router-link to="/hyunsoo">hyunsoo 이동</router-link></h3>
     </div>
   </div>
 </template>
 
 <script setup>
 import Topbar from '@/components/Topbar.vue';
-import { ref } from 'vue';
-import 'v-calendar/style.css';
-
-const date = ref(null);
 </script>
 
 <style scoped>
+body {
+  margin: 0;
+  font-family: Arial, sans-serif;
+}
+
+.topbar {
+  margin-bottom: 20px;
+}
+
+.calendarTopbar {
+  margin-bottom: 20px;
+  text-align: center;
+}
+
 .calendar-container {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 1000px%;
+  margin-top: 70px;
+  border: 1px solid black;
+}
+
+.v-date-picker {
   width: 1000px;
   height: 1000px;
 }
