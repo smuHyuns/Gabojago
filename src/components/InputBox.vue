9ab75340b3136227d6d3cc5842632975bb9342c4
Isu Kim <gimeus@naver.com>
2024-06-11 16:21:48 +0900

    20240611 여행등록하기> 여행제목페이지 완성 및 InputBox 컴포넌트 제작완료

---
+++ src/components/InputBox.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="box">
+    <div class="header-input">
+      <input class="inputBox" :placeholder="TextInput" />
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from 'vue';
+const props = defineProps({
+  headerInput: String,
+  TextInput: String,
+});
+</script>
+
+<style scoped>
+:root {
+  --grey-0: #ffffff;
+  --grey-100: #fbfbfc;
+  --grey-200: #f5f6f7;
+  --grey-300: #eaecef;
+  --grey-400: #caced4;
+  --grey-600: #8892a0;
+  --grey-700: #616b79;
+  --grey-800: #3e444e;
+  --grey-900: #353b43;
+  --grey-950: #16181c;
+}
+
+.box {
+  width: 1080px;
+  max-height: 700px;
+  text-align: center;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 30px;
+}
+
+.header-input {
+  display: flex;
+  align-items: center;
+  width: 1080px;
+  height: 184px;
+}
+
+.inputBox {
+  width: 965px;
+  height: 138px;
+  background: #f5f6f7;
+  border-radius: 28.8px;
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 400;
+  font-size: 46px;
+  line-height: 1.3;
+  text-align: left;
+  color: #caced4; /* 기본 상태 색상 */
+  padding: 0 20px;
+  border: none; /* Remove border when not focused */
+}
+
+.inputBox::placeholder {
+  color: #caced4; /* 플레이스홀더 색상 */
+}
+
+.inputBox:focus {
+  outline: none;
+  color: #353b43; /* 포커스 상태 색상 */
+}
+</style>
