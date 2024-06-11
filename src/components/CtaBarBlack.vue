decb853eb3f0c63edfc7396288cbf3fd96f8ab4a
김시완 <rkdlqdh112@naver.com>
2024-06-11 15:18:05 +0900

    240611 ctabar검정색

---
+++ src/components/CtaBarBlack.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <button class="cta-bar" @click="addSomething">{{ inputname }}</button>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+
+const props = defineProps({
+  inputname: String,
+});
+
+function addSomething() {
+  console.log(props.inputname);
+}
+</script>
+
+<style scoped>
+.cta-bar {
+  width: 375px;
+  height: 77px;
+  padding: 12px 16px;
+  background: #3e444e;
+  border-radius: 10px;
+  /* 글자 관련 CSS 속성 */
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 16px;
+  line-height: 1.3;
+  text-align: center; /* 글자 수평 중앙 정렬 */
+}
+</style>
--- src/pages/Siwan.vue
+++ src/pages/Siwan.vue
@@ -1,10 +1,75 @@
 <template>
-  <div>
-    <h1>시완페이지</h1>
-    <CtaBar inputname="click me!" />
+  <div class="page-container">
+    <div class="top">여행자님의<br />닉네임을 알려주세요!</div>
+    <input
+      type="text"
+      placeholder="10자 이내로 입력해주세요."
+      class="middle"
+      v-model="inputname"
+    />
+
+    <CtaBar class="down" inputname="click me!" />
   </div>
 </template>
 
 <script setup>
-import CtaBar from "@/components/CtaBar.vue";
+import CtaBar from "@/components/CtaBarBlack.vue";
 </script>
+
+<style scoped>
+.top {
+  font-family: Pretendard;
+  /* 여행자님의 닉네임 알려주세요! (text) */
+  width: 194px;
+  height: 72px;
+
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 700;
+  font-size: 26px;
+  line-height: 1.4;
+  text-align: left;
+  transform: translateX(-50%);
+  position: relative;
+  left: 50%;
+
+  color: #353b43;
+}
+.middle {
+  width: 300px;
+  height: 60px;
+  padding: 18px 20px;
+
+  background: #f5f6f7;
+
+  border-radius: 16px;
+
+  /* 10자 이내로 입력해주세요 (text) */
+  width: 184px;
+  height: 23px;
+
+  font-family: Pretendard;
+  font-style: normal;
+  font-weight: 500;
+  font-size: 18px;
+  line-height: 1.3;
+  text-align: center;
+  left: 50%;
+  position: relative;
+  transform: translateX(-50%);
+
+  color: #000;
+}
+.page-container {
+  height: inherit;
+  position: relative;
+}
+
+.down {
+  /* 아래는 수직으로 중앙 정렬을 위한 코드 */
+  position: absolute;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+</style>
