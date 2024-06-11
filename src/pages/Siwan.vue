558f10a722ed3bf8e5d7aa2cb44694c531a92dfe
김시완 <rkdlqdh112@naver.com>
2024-06-11 16:30:55 +0900

    240611 작업저장용

--- src/pages/Siwan.vue
+++ src/pages/Siwan.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="page-container">
-    <div class="top">여행자님의<br />닉네임을 알려주세요!</div>
-    <input
-      type="text"
-      placeholder="10자 이내로 입력해주세요."
-      class="middle"
-      v-model="inputname"
-    />
+    <div class="container">
+      <div class="top">여행자의<br />닉네임을 적어주세요!</div>
+      <input
+        type="text"
+        placeholder="10자 이내로 입력해주세요."
+        class="middle"
+        v-model="inputname"
+        @input="limitInput"
+      />
+    </div>
 
     <CtaBar class="down" inputname="click me!" />
   </div>
@@ -14,50 +17,50 @@
 
 <script setup>
 import CtaBar from "@/components/CtaBarBlack.vue";
+
+let inputname = ""; // 데이터 바인딩을 위한 변수
+
+// 입력 제한 함수
+const limitInput = (event) => {
+  if (event.target.value.length > 10) {
+    event.target.value = event.target.value.slice(0, 10); // 10자 이후의 문자열을 제거
+  }
+  inputname = event.target.value; // 데이터 업데이트
+};
 </script>
 
 <style scoped>
+.container {
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-start;
+  align-items: center;
+  margin-top: 20vh;
+}
 .top {
   font-family: Pretendard;
-  /* 여행자님의 닉네임 알려주세요! (text) */
-  width: 194px;
-  height: 72px;
-
-  font-family: Pretendard;
+  width: 100%;
   font-style: normal;
   font-weight: 700;
   font-size: 26px;
   line-height: 1.4;
-  text-align: left;
-  transform: translateX(-50%);
-  position: relative;
-  left: 50%;
-
+  text-align: center;
   color: #353b43;
 }
 .middle {
   width: 300px;
   height: 60px;
   padding: 18px 20px;
-
+  margin-top: 20px;
   background: #f5f6f7;
-
   border-radius: 16px;
-
-  /* 10자 이내로 입력해주세요 (text) */
-  width: 184px;
-  height: 23px;
-
   font-family: Pretendard;
   font-style: normal;
   font-weight: 500;
   font-size: 18px;
   line-height: 1.3;
   text-align: center;
-  left: 50%;
-  position: relative;
-  transform: translateX(-50%);
-
   color: #000;
 }
 .page-container {
@@ -66,7 +69,6 @@ import CtaBar from "@/components/CtaBarBlack.vue";
 }
 
 .down {
-  /* 아래는 수직으로 중앙 정렬을 위한 코드 */
   position: absolute;
   bottom: 30px;
   left: 50%;
