141cde07f0b51fdd2c8a1864439a6edd08e51ac7
김시완 <rkdlqdh112@naver.com>
2024-06-13 12:06:39 +0900

    240613 도쿄캘린더 제작(미완성)

--- src/pages/Siwan_tour.vue
+++ src/pages/Siwan_tour.vue
@@ -1,14 +1,14 @@
 <template>
   <div></div>
   <div>
-    <DeleteButton-siwan
+    <HistoryListItem
       list="관광"
       number="-5,109"
       number2="-580"
       img="/src/assets/관광.png"
     />
 
-    <DeleteButton-siwan
+    <HistoryListItem
       list="현금"
       number="+1,000,000"
       number2="+11,392.77"
@@ -21,8 +21,8 @@
 // import TopbarXSign test from "@/components/TopbarXSign.vue";
 
 // import Tokyo from "@/components/Tokyo.vue";
-// import HistoryListItem from "@/components/HistoryListItem.vue";
-import DeleteButton-siwan from "@/componets/DeleteButton-siwan.vue";
+import HistoryListItem from "@/components/HistoryListItem.vue";
+// import DeleteButton-siwan from "@/componets/DeleteButton-siwan.vue";
 </script>
 
 <style scoped></style>
