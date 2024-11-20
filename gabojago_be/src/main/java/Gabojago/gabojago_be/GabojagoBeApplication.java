511d566aefa60d0b5fd16cbff10a5e67954bf69b
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-11-20 20:25:00 +0900

    feat : intial setting

---
+++ gabojago_be/src/main/java/Gabojago/gabojago_be/GabojagoBeApplication.java
@@ -0,0 +1,13 @@
+package Gabojago.gabojago_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GabojagoBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GabojagoBeApplication.class, args);
+	}
+
+}
---
+++ gabojago_be/src/test/java/Gabojago/gabojago_be/GabojagoBeApplicationTests.java
@@ -0,0 +1,13 @@
+package Gabojago.gabojago_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GabojagoBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
