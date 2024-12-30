44cc91f3ad2b6d17e48360195579ef0734f4c2ae
최현수 <106013493+smuHyuns@users.noreply.github.com>
2024-12-30 13:49:57 +0900

    hotfix : build를 위한 cors 수정

--- gabojago_be/src/main/java/Gabojago/gabojago_be/scheduler/ExchangeRateScheduler.java
+++ gabojago_be/src/main/java/Gabojago/gabojago_be/scheduler/ExchangeRateScheduler.java
@@ -2,16 +2,19 @@ package Gabojago.gabojago_be.scheduler;
 
 import Gabojago.gabojago_be.exchangeRate.ExchangeRateService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 @Component
+@Slf4j
 @RequiredArgsConstructor
 public class ExchangeRateScheduler {
     private final ExchangeRateService exchangeRateService;
 
     @Scheduled(cron = "0 5 9 * * ?", zone = "Asia/Seoul")
     public void scheduleUpdate() {
+        log.info("ExchangeRate Update 시작");
         exchangeRateService.updateExchangeRate();
     }
 }
