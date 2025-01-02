1fd31021f1c27678267a7945a42d9a7eb59ccbe4
최현수 <106013493+smuHyuns@users.noreply.github.com>
2025-01-02 14:58:35 +0900

    chore(App.vue) : 초기화면실행시 확대됨을 막기위한 설정

--- gabojago_fe/src/App.vue
+++ gabojago_fe/src/App.vue
@@ -25,9 +25,9 @@
   background-color: transparent;
 }
 
-/* #app {
+#app {
   margin: 0;
-} */
+}
 
 @font-face {
   font-family: 'Pretendard-Regular';
@@ -37,37 +37,7 @@
   font-style: normal;
 }
 
-/*  */
-html,
-body {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  overflow: hidden; /* 스크롤 방지 */
-}
-
-#app {
-  width: 100%;
-  height: 100%;
-  margin: 0;
-}
-
 .mainContainer {
-  width: 100vw; /* 뷰포트 너비를 100% 사용 */
-  height: 100vh; /* 뷰포트 높이를 100% 사용 */
-  background-color: white;
-  display: flex;
-  justify-content: center; /* 중앙 정렬 */
-  align-items: center; /* 중앙 정렬 */
-}
-
-.container {
-  width: 100%; /* 부모 요소 크기에 맞춤 */
-  height: 100%; /* 부모 요소 크기에 맞춤 */
-}
-
-/* .mainContainer {
   max-width: 1080px;
   max-height: 2340px;
   background-color: white;
@@ -76,5 +46,5 @@ body {
 .container {
   width: 1080px;
   height: 2340px;
-} */
+}
 </style>
